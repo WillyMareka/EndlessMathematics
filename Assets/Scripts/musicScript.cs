7773f069bb611b38999cbe6578163e08b87bec9f
Willy Mareka Michael <marekawilly@gmail.com>
2017-01-02 23:51:19 +0300

    Now music is on multiple pages....now the settings on/off and pause back button after death

---
+++ Assets/Scripts/musicScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class musicScript : MonoBehaviour {
+
+	Scene sn;
+
+	private static musicScript instance = null;
+	public static musicScript Instance{
+		get { return instance; }
+	}
+
+	void Awake(){
+		if(instance != null && instance != this){
+			Destroy(this.gameObject);
+		}else{
+			instance = this;
+		}
+	}
+
+	void Update(){
+		if (this.gameObject) {
+			sn = SceneManager.GetActiveScene ();
+			if (sn.name == "testlevel") {
+				Destroy (this.gameObject);
+			} else {
+				DontDestroyOnLoad (this.gameObject);
+			}
+		}
+	}
+
+
+}
